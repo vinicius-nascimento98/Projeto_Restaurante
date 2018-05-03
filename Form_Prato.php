61cd25ebe6805e48e502145953b48dbf27c71317
vinicius-nascimento98 <38798347+vinicius-nascimento98@users.noreply.github.com>
2018-05-02 22:26:21 -0300

    Delete Form_Prato.php
    Apagou arquivo Form_Prato.php

--- Form_Prato.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('Cabecalho/Cabecalho.php'); //mudar para cabecalho geral
-include('Class/ClassForm.php');
-
-$inp1 = array("label"=>"Nome do Prato", "nome"=>"descricao", "tipo"=>"text");
-
-?>
