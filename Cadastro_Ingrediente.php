f3ff4ccd4ad5b5a19a2eb97edcbcc165d2a896fa
daviF-Freitas <ffdavi@homtmail.com>
2018-05-09 17:15:31 -0300

    cadastro ingrediente atualizado

--- Cadastro_Ingrediente.php
+++ Cadastro_Ingrediente.php
@@ -1,23 +1,15 @@
 <?php
 if(!empty($_POST)){
 	include("Cabecalho/Cabecalho.php");
-	include('Conexao.php');
-
-$insert = "INSERT INTO ingrediente (nome_Ingrediente, custo, estoque) values (:nome_Ingrediente, :custo, :estoque);";
-
-$stmt = $conn->prepare($insert);
-
-$nome_Ingrediente = $_POST['nome_Ingrediente']; 
-$custo = $_POST['custo'];
-$estoque = $_POST['estoque'];
-
-$stmt->bindValue(':nome_Ingrediente',$nome_Ingrediente);
-$stmt->bindValue(':custo',$custo);
-$stmt->bindValue(':estoque',$estoque);
-
-$stmt->execute();
-
-echo "Ingrediente cadastrado com sucesso!";
+	include("Class/ClassBD.php");
+	include("Conexao.php");
+	
+	$i = new BD($conn);
+	
+	$i->insert($_POST, "ingrediente");
+	
+	echo "<br/>";
+	echo "Ingrediente cadastrado com sucesso!";
 
 }
 else{
