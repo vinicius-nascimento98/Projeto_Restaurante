9a9a9ba2260c859bfdc612236b48bed8ce73f929
gmlessi <gmlessi@hotmail.com>
2018-05-11 15:25:17 -0300

    Código identado corretamente e com espaços adequados - boas praticas

--- Cadastro_Atendente.php
+++ Cadastro_Atendente.php
@@ -2,20 +2,20 @@
 	include("Cabecalho/Cabecalho.php");
 	include("Class/ClassBD.php");
 	
-if(!empty($_POST)){
-	include("Conexao.php");
+	if(!empty($_POST)){
+		include("Conexao.php");
 
-	$a = new BD($conn);
+		$a = new BD($conn);
 
-	$a->insert($_POST,"atendente");
+		$a->insert($_POST,"atendente");
 
-	echo "<br/>";
-	echo "Atendente Cadastrado com Sucesso";
-	
-	//echo "<br /> <a href='Form_CadastroAtendente.php'> Cadastrar novo Atendente </a>";
-}	
-else{
-	header("location: Form_Atendente.php");
-}
+		echo "<br/>";
+		echo "Atendente Cadastrado com Sucesso";
+		
+		//echo "<br /> <a href='Form_CadastroAtendente.php'> Cadastrar novo Atendente </a>";
+	}	
+	else{
+		header("location: Form_Atendente.php");
+	}
 
 ?>
