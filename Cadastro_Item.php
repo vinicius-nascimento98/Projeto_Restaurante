3a72d4e4916a529409471210ec52084ff53d76af
gmlessi <gmlessi@hotmail.com>
2018-05-11 15:26:57 -0300

    Código identado corretamente e com espaços adequados - boas praticas

--- Cadastro_Item.php
+++ Cadastro_Item.php
@@ -1,18 +1,20 @@
 <?php
-if(!empty($_POST)){
-	include("Cabecalho/Cabecalho.php");
-	include("Class/ClassBD.php");
-	include("Conexao.php");
 
-	$item = new BD($conn);
-	
-	$item->insert($_POST, "item");
-	
-	echo "<br />";
-	echo "Item cadastrado com sucesso!!";
-	
-}else{
-	header("location: Form_Item.php");
-}
+	if(!empty($_POST)){
+		include("Cabecalho/Cabecalho.php");
+		include("Class/ClassBD.php");
+		include("Conexao.php");
+
+		$item = new BD($conn);
+		
+		$item->insert($_POST, "item");
+		
+		echo "<br />";
+		echo "Item cadastrado com sucesso!!";
+		
+	}
+	else{
+		header("location: Form_Item.php");
+	}
 
 ?>
