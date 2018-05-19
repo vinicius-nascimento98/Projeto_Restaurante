d8d1bbc824f042f81cff5ae3969a6cdb869a9a05
gmlessi <gmlessi@hotmail.com>
2018-05-18 22:13:01 -0300

    Alteração: Cadastro de mesa

--- Class/Model/ClassMesa.php
+++ Class/Model/ClassMesa.php
@@ -2,34 +2,21 @@
 
 	class Mesa{
 		protected $id_mesa;
-		protected $status_mesa;
-		
 		
 		public function __construct($mesa){
 			if(isset($mesa["id_mesa"])){
 				$this->set_id_mesa($mesa["id_mesa"]);
-			}
-			$this->set_status_mesa($mesa["status_mesa"]);
-			
+			}	
 		}
 		
 		public function set_id_mesa($id_mesa){
 			$this->id_mesa = $id_mesa;
 		}
 		
-		public function set_status_mesa($status_mesa){
-			$this->status_mesa = $status_mesa;
-		}
-		
 		public function get_id_mesa(){
 			return $this->id_mesa;
 		}
 		
-		public function get_status_mesa(){
-			return $this->status_mesa;
-		}
-		
 	}
 		
-
 ?>
