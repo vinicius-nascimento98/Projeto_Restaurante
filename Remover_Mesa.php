e2430ad3c9504738d7a0ea7d8b6e86fb8e11f8ca
gmlessi <gmlessi@hotmail.com>
2018-05-23 18:25:52 -0300

    Alteração da remover mesa para remover a mesa maior

--- Remover_Mesa.php
+++ Remover_Mesa.php
@@ -6,6 +6,7 @@
 	$m = new BD($conn);
 	
 	$remover_mesa = $m->select("vw_max_mesa");
+	$id_mesa = $remover_mesa["id_mesa"];
 	
-	header("Location: Remover.php?id=$remover_mesa&tabela=mesa");
+	header("Location: Remover.php?id=$id_mesa&tabela=mesa");
 ?>
